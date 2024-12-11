a10b6f471ccbfb4060fb844eb1590f16028b5305
Pratik Trivedi <pratiktrivedi719@gmail.com>
2024-12-11 23:00:12 +0530

    Fix the error

---
+++ types/tailwindcss.d.ts
@@ -0,0 +1,4 @@
+declare module "tailwindcss/lib/util/flattenColorPalette" {
+    // Adjust the type below based on the actual usage
+    export default function flattenColorPalette(colors: unknown): Record<string, string>;
+  }
